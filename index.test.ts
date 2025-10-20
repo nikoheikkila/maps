3a33a5550573f8a765d6332beaccb833ee2ecb09
Niko Heikkilä <niko@heikkila.io>
2025-10-20 16:39:12 +0300

    feat: adds a new record

--- index.test.ts
+++ index.test.ts
@@ -1,11 +1,38 @@
 import { describe, it, expect } from 'bun:test';
 
-class TimeSeriesMap extends Map {}
+type KeyFunction = () => number;
+
+class TimeSeriesMap<T> extends Map<number, T> {
+  private readonly keyFn: KeyFunction;
+
+  public constructor(keyFn: KeyFunction = Date.now) {
+    super();
+
+    this.keyFn = keyFn;
+  }
+
+  public add(data: T): number {
+    const key = this.keyFn();
+    this.set(key, data);
+    return key;
+  }
+}
 
 describe('Time Series Map', () => {
   it('instantiates an empty map', () => {
     const map = new TimeSeriesMap();
 
     expect(map.size).toBe(0);
+  });
+
+  it('adds a new record and returns the key', () => {
+    const expectedKey = 1;
+    const expectedValue = 'data';
+    const map = new TimeSeriesMap(() => expectedKey);
+
+    const key = map.add(expectedValue);
+
+    expect(key).toBe(expectedKey);
+    expect(map.get(key)).toBe(expectedValue);
   });
 });
